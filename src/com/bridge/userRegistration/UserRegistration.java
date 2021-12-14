3dd97672515fe7e2897c3423e4d7b8cee7a2d140
Navneet <navn0306@gmail.com>
2021-12-15 02:22:59 +0530

    ADD: Feature to check valid Fname anf Lname with Starting with Uppercase and have atleast three characters

--- src/com/bridge/userRegistration/UserRegistration.java
+++ src/com/bridge/userRegistration/UserRegistration.java
@@ -1,24 +1,12 @@
 package com.bridge.userRegistration;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class UserRegistration {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter FName");
-        String fName = sc.nextLine();
-        String regex = "^[A-Z][a-zA-Z]{2,}";
 
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(fName);
 
-        if (matcher.matches()) {
-            System.out.println("Valid FName");
-        } else {
-            System.out.println("Invalid FName");
-        }
+        ReadData.fName();
+        ReadData.lName();
+
     }
 }
