e9160adbb5e5211ebe1319c1e1c58ce005acbec4
Navneet <navn0306@gmail.com>
2021-12-16 23:38:14 +0530

    ADD: UC8, Regex to validate only one special char in password

---
+++ src/main/java/com/bridge/userRegistration/ReadData.java
@@ -0,0 +1,73 @@
+package com.bridge.userRegistration;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReadData {
+
+    public static String fName(String fName) {
+
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(fName);
+
+        if (matcher.matches())
+            return "Valid FName";
+
+        return "Not Valid";
+
+    }
+
+    public static String lName(String lName) {
+
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lName);
+
+        if (matcher.matches())
+            return "Valid LName";
+
+        return "Not Valid";
+    }
+
+    public static String email(String email) {
+
+        String regex = "^[a-z0-9]{3,20}([_.+-][a-z0-9]+)?@[a-z0-9]+.[a-z]{2,3}(.[a-z]{2})?$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (matcher.matches())
+            return "Valid Email";
+
+        return "Not Valid";
+    }
+
+    public static String mobile(String mobile) {
+
+        String regex = "^(91[ ])?[6-9]\\d{9}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(mobile);
+
+        if (matcher.matches())
+            return "Valid Mobile Number";
+
+        return "Not Valid";
+    }
+
+    public static String password(String password) {
+
+        String regex = "(?=.*[A-Z])([@#$%])$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+
+        if (matcher.matches())
+            return "Valid Password";
+
+        return "Not Valid";
+    }
+}
---
+++ src/main/java/com/bridge/userRegistration/UserRegistration.java
@@ -0,0 +1,14 @@
+package com.bridge.userRegistration;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+
+        ReadData.fName("Navneet");
+        ReadData.lName("Gautam");
+        ReadData.email("abc.xyz@bl.co.in");
+        ReadData.mobile("91 9066299900");
+        ReadData.password("Abc@123");
+
+    }
+}
