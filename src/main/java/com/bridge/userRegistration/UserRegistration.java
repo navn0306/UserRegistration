67e5b6af2de2410cb46f134264e2111872f6e245
Navneet <navn0306@gmail.com>
2021-12-22 01:48:35 +0530

    Refactor: Removed unnecessary lines of code.

--- src/main/java/com/bridge/userRegistration/UserRegistration.java
+++ src/main/java/com/bridge/userRegistration/UserRegistration.java
@@ -4,14 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public String VALIDATE_FNAME = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
-    public String VALIDATE_LNAME = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
-    public String VALIDATE_EMAIL = "^[a-z0-9]{3,20}([_+.-][a-z0-9]+)?@[a-z0-9]+.[a-z]{2,3}(.[a-z]{2})?$";
-    public String VALIDATE_PHONE = "^[1-9]{2}( )[0-9]{10}$";
-    public String VALIDATE_PASSWORD = "^([a-zA-Z0-9])?.{8,}$";
 
     UserRegistrationInterface validateFname = (fName) -> {
-        this.VALIDATE_FNAME = fName;
         String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(fName);
@@ -21,11 +15,9 @@ public class UserRegistration {
         } else {
             throw new UserRegistrationException("Enter valid FName", UserRegistrationException.ExceptionType.INVALID);
         }
-
     };
 
     UserRegistrationInterface validateLname = (lName) -> {
-        this.VALIDATE_LNAME = lName;
         String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(lName);
@@ -35,12 +27,10 @@ public class UserRegistration {
         } else {
             throw new UserRegistrationException("Enter valid lName", UserRegistrationException.ExceptionType.INVALID);
         }
-
     };
 
 
     UserRegistrationInterface validateEmail = (email) -> {
-        this.VALIDATE_EMAIL = email;
         String regex = "^[a-z0-9]{3,20}([_+.-][a-z0-9]+)?@[a-z0-9]+.[a-z]{2,3}(.[a-z]{2})?$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
@@ -50,11 +40,9 @@ public class UserRegistration {
         } else {
             throw new UserRegistrationException("Enter valid email", UserRegistrationException.ExceptionType.INVALID);
         }
-
     };
 
     UserRegistrationInterface validatePhone = (phone) -> {
-        this.VALIDATE_PHONE = phone;
         String regex = "^[1-9]{2}( )[0-9]{10}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(phone);
@@ -64,11 +52,9 @@ public class UserRegistration {
         } else {
             throw new UserRegistrationException("Enter valid Phone", UserRegistrationException.ExceptionType.INVALID);
         }
-
     };
 
     UserRegistrationInterface validatePassword = (password) -> {
-        this.VALIDATE_PASSWORD = password;
         String regex = "^([a-zA-Z0-9])?.{8,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
@@ -80,6 +66,4 @@ public class UserRegistration {
         }
 
     };
-
-
 }
